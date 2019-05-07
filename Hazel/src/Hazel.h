f67ddb3a45913a3f78f8fa87366613a33f6b83c8
Ofek Shimon <2000ofek20@gmail.com>
2019-05-07 21:41:53 +0300

    Setup basic application and entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For Hazel applications
+
+#include "Hazel/Application.h"
+
+// Entry Point ---------------------------------
+#include "Hazel/EntryPoint.h"
+// ---------------------------------------------
