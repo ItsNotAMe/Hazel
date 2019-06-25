6f4cddf4d38348e7bcb0126db80c82e3c13a3393
Ofek Shimon <2000ofek20@gmail.com>
2019-06-25 22:50:43 +0300

    Added the RendererAPI, RenderCommand and changed the Renderer

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,9 +1,9 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include <glad/glad.h>
-
 #include "Input.h"
+
+#include "Renderer/Renderer.h"
 
 namespace Hazel {
 
@@ -173,16 +173,18 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
-			glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
+			RenderCommand::SetClearColor({ 0.15f, 0.15f, 0.15f, 1.0f });
+			RenderCommand::Clear();
+
+			Renderer::BeginScene();
 
 			m_BlueShader->Bind();
-			m_SquareVA->Bind();
-			glDrawElements(GL_TRIANGLES, m_SquareVA->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_SquareVA);
 
 			m_Shader->Bind();
-			m_VertexArray->Bind();
-			glDrawElements(GL_TRIANGLES, m_VertexArray->GetIndexBuffer()->GetCount(), GL_UNSIGNED_INT, nullptr);
+			Renderer::Submit(m_VertexArray);
+
+			Renderer::EndScene();
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
